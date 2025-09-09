e41eeec8b4ff316788ea8079358cfb290ddfdd75
jsb999 <90780236+jsb999@users.noreply.github.com>
2025-09-09 21:37:51 +0000

    code transfered

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,12 +1,41 @@
 package org.example;
 
+import java.util.Scanner
+
 public class App {
-  public int getInput() {
-    // Implement your function here
-    return 0;
+  private Scanner userInput = new Scanner(System.in);//scanner for the user inputs
+  
+  public static void main(String[] args) {
+int userValue = getInput(1, 100, "Please enter a number between 1 and 100:", "that was not a valid input, try again,");//first initial call of get input 
+    System.out.println("You have inputed: " + userValue);//outputs the inputed value if it is correct
+
+    userValue = getInput(1, 12, "Please enter a number between 1 and 12:", "that was not a valid input, try again,"); //second call of get input
+    System.out.println("You have inputed: " + userValue); //outputs the inputed value if it is correct
   }
 
-  public static void main(String[] args) {
-    // Test your function here in a driver program
+  public int getInput(int lowBound, int highBound, String inputPrompt, String errorPrompt){
+    boolean validInput = false;
+    int userValue = 0;
+
+
+    while(!validInput){
+      System.out.println(inputPrompt);
+
+      if(!userInput.hasNextInt()){ //checks if the input is an integer
+        validInput = false;
+        System.out.println(errorPrompt);
+        userInput.next();
+      } else{
+        userValue = userInput.nextInt();
+        if(userValue < lowBound || userValue > highBound){//checks if the input is within the bounds
+          validInput = false;
+          System.out.println(errorPrompt);
+        } else{
+          validInput = true;
+        }
+      }
+    }
+
+    return userValue; //returns the inputed value if it is correct
   }
 }
